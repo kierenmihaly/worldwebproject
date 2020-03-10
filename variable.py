65c9147d4b26a817ccda6f7db0d6f31a3f57190e
kierenmihaly <51211386+kierenmihaly@users.noreply.github.com>
2020-03-10 21:49:16 +0900

    09032020 1day 1commit

---
+++ variable.py
@@ -0,0 +1,72 @@
+
+identity = '지구인'
+print(identity)
+
+
+주석
+# 주석
+
+여러줄 주석
+
+'''
+주석
+'''
+
+"""
+주석
+"""
+
+숫자와 문자열
+
+my_name = 'Python'
+my_age = '25'
+
+print(my_name,)
+
+my_next_age = ma_age + 1
+
+print('내년에는', my_next_age, '삶')
+
+숫자 ; 수학 연산이 가능 + - * / %
+"문자열" ; 화면에 그대로 출력 가능, 따옴표로 둘러싸서 표시
+
+예제
+
+birth_year='1999'
+birth_date='0326'
+year_and_date =
+
+print("year_and_date : {}".format(year_and_date))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+조건문
