d5f1e4f98a1d1242770ce1a1f4951f097ddc362a
kierenmihaly <51211386+kierenmihaly@users.noreply.github.com>
2020-03-19 18:59:05 +0900

    19032020_1day1commit

---
+++ Test2.java
@@ -0,0 +1,18 @@
+public class Test2
+{
+	public static void main(String[] args)
+	{
+		int i = Integer.parseInt(args [0]);
+		int j = Integer.parseInt(args [1]);
+
+if (i > j) {
+	System.out.println("첫번째인수:"+i+",두번째인수:"+j+","+i+"-"+j+"="+i);
+}
+	else  {
+		System.out.println("첫번째인수:"+i+",두번째인수:"+j+","+i+"+"+j+"="+(i+j));
+	}
+
+
+	} //end of main
+
+} //end of class 
