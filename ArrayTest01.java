d6ed39b97877307d4388404abe5bf83a2c105a24
kierenmihaly <51211386+kierenmihaly@users.noreply.github.com>
2020-04-02 17:11:38 +0900

    02042020_1day1commit

--- ArrayTest01.java
+++ ArrayTest01.java
@@ -14,16 +14,19 @@ public class ArrayTest01{
 	intArray[7]=8;
 	intArray[8]=9;
 
-	
+	//배열의 index를 이용 각각의 value에 접근 
 	for( int j = 0; j < 9; j++){
 		System.out.println("intArray["+j+"] 값은 : "+intArray[j]);
 	}
 
+	//각각의 값을 이용하여 5단을 출력 intArray.length
+	//Operator 와 length keyword 사용 
 	System.out.println("==================");
 	for( int k = 0; k<intArray.length; k++) {
-		System.out.println(" 5 X" +intArray[k]+"="+5*intArray[k]);
+		System.out.println(" 5 X " +intArray[k]+"="+5*intArray[k]);
 	}
 
+	//아래의 출력 내용을 이해하자 (primitive int i =0;과 비교 이해하면됨) 
 	int [] j = intArray;
 	System.out.println("==================");
 	System.out.println("j[0]의 값은 :" +j[0]);
--- ForTest.java
+++ ForTest.java
@@ -2,17 +2,19 @@
 public class  ForTest{
 	
 	public static void main(String[] args) {
+	
 
+		
 		int j = 5;	
 		System.out.println(j+"단을 출력합니다.");
 
-		for (int i=1 ; 1<10 ; i++ ){
+		for (int i=1 ; i<10 ; i++ ){
 				System.out.println(j+"*"+i+"="+j*i);
 	}
 
-		for (; ; ) {
-			System.out.println("여기는 반복문내부의 무한 루프");
+		//for ( ; ; ) {
+			//System.out.println("여기는 반복문내부의 무한 루프");
 		}
-	
-	}
-}
+		//System.out.println("error가 발생한다. 이유는");
+
+	}//main
