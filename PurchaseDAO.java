e4be08a22ad5f60f623ed18c3b769c18b78e1bcb
kierenmihaly <51211386+kierenmihaly@users.noreply.github.com>
2020-05-17 16:13:58 +0900

    14052020_1day1commit

--- PurchaseDAO.java
+++ PurchaseDAO.java
@@ -5,148 +5,220 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.model2.mvc.common.Search;
 import com.model2.mvc.common.util.DBUtil;
-import com.model2.mvc.service.domain.Purchase;
-
+import com.model2.mvc.service.domain.*;
+import com.model2.mvc.service.product.ProductService;
+import com.model2.mvc.service.product.impl.ProductServiceImpl;
+import com.model2.mvc.service.user.UserService;
+import com.model2.mvc.service.user.impl.UserServiceImpl;
 
 public class PurchaseDAO {
-
+	
 	public PurchaseDAO() {
 	}
-
-	public void insertPurchase(Purchase purchase) throws Exception {
-
+	
+	public void insertPurchase(Purchase purchase) throws Exception{
+				
 		Connection con = DBUtil.getConnection();
-		// 실제 데이터베이스에 쿼리를 보내기위해
-		String sql =  "INSERT "+
-				"INTO PURCHASES"+
-				"VALUES (seq_transaction_tran_no.nextval,?,?,?,?,?,0,sysdate,?)";
+		
+		String sql="INSERT INTO transaction"
+				+ " VALUES (seq_transaction_tran_no.nextval,?,?,?,?,?,?,?,?,SYSDATE,?)";
 		PreparedStatement stmt = con.prepareStatement(sql);
-		stmt.setString(1, purchase.getBuyer().getUserId());
-		stmt.setString(2, purchase.getPaymentOption());
-		stmt.setString(3, purchase.getReceiverName());
-		stmt.setString(4, purchase.getReceiverPhone());
-		stmt.setString(5, purchase.getDivyAddr());
-		stmt.setString(6, purchase.getDivyRequest());
-		stmt.setString(7, purchase.getDivyDate());
-		stmt.setInt(8, purchase.getPurchaseProd().getProdNo());
+		stmt.setInt(1, purchase.getPurchaseProd().getProdNo());
+		stmt.setString(2, purchase.getBuyer().getUserId());
+		stmt.setString(3, purchase.getPaymentOption());
+		stmt.setString(4, purchase.getReceiverName());
+		stmt.setString(5, purchase.getReceiverPhone());
+		stmt.setString(6, purchase.getDivyAddr());
+		stmt.setString(7, purchase.getDivyRequest());
+		stmt.setString(8, purchase.getTranCode());
+		stmt.setString(9, purchase.getDivyDate());
 		
 		stmt.executeUpdate();
-		// select문 제외한거 sql문 실행
+		
+		stmt.close();
 		con.close();
-		// statement 객체를 반환할 때 쓰임
+		
 	}
-
-	public Purchase findPurchase(int prodNo) throws Exception {
-
+	
+	public Map<String, Object> getPurchaseList(Search search, String buyerId) throws Exception{
+		
+		Map<String , Object>  map = new HashMap<String, Object>();
+		
 		Connection con = DBUtil.getConnection();
-
-		String sql = "select * from PRODUCT where PROD_NO=?";
-
-		PreparedStatement stmt = con.prepareStatement(sql);
-		stmt.setInt(1, prodNo);
-
-		ResultSet rs = stmt.executeQuery();
-
-		Product product = null;
-		while (rs.next()) {
-			product = new ProductVO();
+		
+		String sql="SELECT * FROM transaction WHERE buyer_id='"+buyerId+"'";
+		
+		int totalCount = this.getTotalCount(sql);
+		System.out.println("UserDAO :: totalCount  :: " + totalCount);
+		
+		sql = makeCurrentPageSql(sql, search);
+		PreparedStatement pStmt = con.prepareStatement(sql);
+		ResultSet rs = pStmt.executeQuery();
+		
+		System.out.println(search);
+		
+		List<Purchase> list = new ArrayList<Purchase>();
+		
+		while(rs.next()) {
+			
+			User user=new User();
+			UserService userUservice=new UserServiceImpl();
+			user=userUservice.getUser(buyerId);
+			
+			Product product=new Product();
+			ProductService productService=new ProductServiceImpl();
 			product.setProdNo(rs.getInt("PROD_NO"));
-			product.setProdName(rs.getString("PROD_NAME"));
-			product.setProdDetail(rs.getString("PROD_DETAIL"));
-			product.setManuDate(rs.getString("MANUFACTURE_DAY"));
-			product.setPrice(rs.getInt("PRICE"));
-			product.setFileName(rs.getString("IMAGE_FILE"));
-			product.setRegDate(rs.getDate("REG_DATE"));
+			product=productService.getProduct(product.getProdNo());
+			
+			Purchase purchase=new Purchase();
+			purchase.setBuyer(user);
+			purchase.setReceiverName(rs.getString("receiver_name"));
+			purchase.setReceiverPhone(rs.getString("receiver_phone"));
+			purchase.setPurchaseProd(product);
+			purchase.setTranNo(rs.getInt("TRAN_NO"));
+			purchase.setTranCode(rs.getString("tran_status_code"));
+			list.add(purchase);
+			
+			System.out.println(purchase);
 		}
-
-		con.close();
-
-		return product;
-	}
-
-	public HashMap<String, Object> getPurchaseList(Search search) throws Exception {
-		// haspmap은 map을 구현하고 key value 로 이루어져있음
-		System.out.println("--------------getPurchaseList-----------");
-		Connection con = DBUtil.getConnection();
-
-		String sql = "select * from PURCHASE ";
-		if (search.getSearchCondition() != null) {
-			if (search.getSearchCondition().equals("0")) {
-				sql += " where PROD_NO='" + search.getSearchKeyword() + "'";
-			} else if (search.getSearchCondition().equals("1")) {
-				sql += " where PROD_NAME='" + search.getSearchKeyword() + "'";
-			} else if (search.getSearchCondition().equals("2")) {
-				sql += " where PRICE='" + search.getSearchKeyword() + "'";
-			}
-		}
-		sql += " order by PROD_NO";
-
-		PreparedStatement stmt = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
-				// 한번 커서가 지나간 다음에 다시 되돌릴수 없다
-				ResultSet.CONCUR_UPDATABLE);
-		// resultset으로 가져온 row의 값을 다시 insert나 업
-		// 데이트로 쓰지 않겠다는것
-
-		ResultSet rs = stmt.executeQuery();
-
-		rs.last();
-		int total = rs.getRow();
-		System.out.println(" ο      :" + total);
-
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("count", new Integer(total));
-
-
-		ArrayList<Product> list = new ArrayList<Product>();
-		if (total > 0) {
-			for (int i = 0; i < search.getPageUnit(); i++) {
-				Product vo = new Product();
-				vo.setProdNo(rs.getInt("PROD_NO"));
-				vo.setProdName(rs.getString("PROD_NAME"));
-				vo.setProdDetail(rs.getString("PROD_DETAIL"));
-				vo.setManuDate(rs.getString("MANUFACTURE_DAY"));
-				vo.setPrice(rs.getInt("PRICE"));
-				vo.setFileName(rs.getString("IMAGE_FILE"));
-				vo.setRegDate(rs.getDate("REG_DATE"));
-
-				list.add(vo);
-				if (!rs.next())
-					break;
-			}
-		}
-		System.out.println("DAO list======> " + list);
-		System.out.println("list.size() : " + list.size());
+		
+		map.put("totalCount", new Integer(totalCount));
 		map.put("list", list);
-		System.out.println("map().size() : " + map.size());
-
+		
+		rs.close();
+		pStmt.close();
 		con.close();
-
+		
 		return map;
 	}
-
-	public void updatePurchase(Purchase purchase) throws Exception {
-
+	
+	public Purchase findPurchase(int tranNo) throws Exception{
+		
 		Connection con = DBUtil.getConnection();
-
-		String sql = "update PRODUCT set PROD_NAME=?,PROD_DETAIL=?,MANUFACTURE_DAY=?,PRICE=?,IMAGE_FILE=? where PROD_NO=?";
-
+		
+		String sql="SELECT * FROM transaction WHERE tran_no=?";
+		
 		PreparedStatement stmt = con.prepareStatement(sql);
-		stmt.setString(1, purchase.getProdName());
-		stmt.setString(2, purchase.getProdDetail());
-		stmt.setString(3, purchase.getManuDate());
-		stmt.setInt(4, purchase.getPrice());
-		stmt.setString(5, purchase.getFileName());
-		stmt.setInt(6, purchase.getProdNo());
+		stmt.setInt(1, tranNo);
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		Purchase purchase=null;
+		
+		while(rs.next()) {
+			
+			User user=new User();
+			UserService userUservice=new UserServiceImpl();
+			user=userUservice.getUser(rs.getString("BUYER_ID"));
+			
+			Product product=new Product();
+			ProductService productService=new ProductServiceImpl();
+			product=productService.getProduct(rs.getInt("prod_no"));
+						
+			purchase=new Purchase();
+			purchase.setBuyer(user);
+			purchase.setDivyAddr(rs.getString("demailaddr"));
+			purchase.setDivyDate(rs.getString("dlvy_date"));
+			purchase.setDivyRequest(rs.getString("dlvy_request"));
+			purchase.setOrderDate(rs.getDate("order_date"));
+			purchase.setPaymentOption((rs.getString("payment_option")));
+			purchase.setPurchaseProd(product);
+			purchase.setReceiverName(rs.getString("receiver_name"));
+			purchase.setReceiverPhone(rs.getString("receiver_phone"));
+			purchase.setTranCode(rs.getString("tran_status_code"));
+			purchase.setTranNo(rs.getInt("tran_no"));	
+		}
+		
+		con.close();
+		
+		System.out.println("purchaseDAO find() purchase : "+purchase);
+		
+		return purchase;
+	}
+	
+	public void updatePurchase(Purchase purchase) throws Exception{
+		
+		Connection con = DBUtil.getConnection();
+		
+		String sql="UPDATE transaction SET payment_option=?, receiver_name=?, "
+				+ "receiver_phone=?, demailaddr=?, dlvy_request=?,"
+				+ "dlvy_date=? WHERE tran_no=?";
+		
+		PreparedStatement stmt = con.prepareStatement(sql);
+		stmt.setString(1, purchase.getPaymentOption());
+		stmt.setString(2, purchase.getReceiverName());
+		stmt.setString(3, purchase.getReceiverPhone());
+		stmt.setString(4, purchase.getDivyAddr());
+		stmt.setString(5, purchase.getDivyRequest());
+		stmt.setString(6, purchase.getDivyDate());
+		stmt.setInt(7, purchase.getTranNo());
 		stmt.executeUpdate();
+		
+		con.close();
 
+	}
+	
+	private int getTotalCount(String sql) throws Exception {
+		sql = "SELECT COUNT(*) "+
+		          "FROM ( " +sql+ ") countTable";
+		
+		Connection con = DBUtil.getConnection();
+		PreparedStatement pStmt = con.prepareStatement(sql);
+		ResultSet rs = pStmt.executeQuery();
+		
+		int totalCount = 0;
+		if( rs.next() ){
+			totalCount = rs.getInt(1);
+		}
+		
+		pStmt.close();
+		con.close();
+		rs.close();
+		
+		return totalCount;
+	}
+	
+	private String makeCurrentPageSql(String sql , Search search){
+		sql = 	"SELECT * "+ 
+					"FROM (		SELECT inner_table. * ,  ROWNUM AS row_seq " +
+									" 	FROM (	"+sql+" ) inner_table "+
+									"	WHERE ROWNUM <="+search.getCurrentPage()*search.getPageSize()+" ) " +
+					"WHERE row_seq BETWEEN "+((search.getCurrentPage()-1)*search.getPageSize()+1) +" AND "+search.getCurrentPage()*search.getPageSize();
+		
+		System.out.println("UserDAO :: make SQL :: "+ sql);	
+		
+		return sql;
+	}
+	
+	public void updateTranCode(Purchase purchase) throws Exception{
+		
+		Connection con = DBUtil.getConnection();
+		
+		if(purchase.getPurchaseProd().getProTranCode().equals("2")) {
+			
+			String sql="UPDATE transaction SET tran_status_code='2'"
+					+ " WHERE prod_no=?";
+		
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, purchase.getPurchaseProd().getProdNo());
+			stmt.executeUpdate();
+					
+		}else if(purchase.getPurchaseProd().getProTranCode().equals("3")) {
+			
+			String sql="UPDATE transaction SET tran_status_code='3'"
+					+ " WHERE tran_no=?";
+			
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, purchase.getTranNo());
+			stmt.executeUpdate();
+		}
+		
 		con.close();
 	}
 
-	public void updateTranCode(Purchase purchase) throws Exception {
-		
-		
-	}
 }
