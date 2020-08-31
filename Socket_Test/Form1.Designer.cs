34578ffbac29a115bb731065c469f930831d28bd
kierenmihaly <51211386+kierenmihaly@users.noreply.github.com>
2020-08-31 10:46:51 +0900

    31082020_1day1commit
    c# Socket Project

---
+++ Socket_Test/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Socket_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIPaddr = new System.Windows.Forms.TextBox();
+            this.port = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.rbServer = new System.Windows.Forms.RadioButton();
+            this.rdClient = new System.Windows.Forms.RadioButton();
+            this.txtRecv = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(544, 264);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "종료";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP주소 :";
+            // 
+            // txtIPaddr
+            // 
+            this.txtIPaddr.Location = new System.Drawing.Point(111, 20);
+            this.txtIPaddr.Name = "txtIPaddr";
+            this.txtIPaddr.Size = new System.Drawing.Size(202, 21);
+            this.txtIPaddr.TabIndex = 2;
+            // 
+            // port
+            // 
+            this.port.AutoSize = true;
+            this.port.Location = new System.Drawing.Point(355, 29);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(50, 12);
+            this.port.TabIndex = 3;
+            this.port.Text = "PORT : ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(411, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(721, 18);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // rbServer
+            // 
+            this.rbServer.AutoSize = true;
+            this.rbServer.Location = new System.Drawing.Point(560, 25);
+            this.rbServer.Name = "rbServer";
+            this.rbServer.Size = new System.Drawing.Size(59, 16);
+            this.rbServer.TabIndex = 10;
+            this.rbServer.TabStop = true;
+            this.rbServer.Text = "Server";
+            this.rbServer.UseVisualStyleBackColor = true;
+            // 
+            // rdClient
+            // 
+            this.rdClient.AutoSize = true;
+            this.rdClient.Location = new System.Drawing.Point(644, 25);
+            this.rdClient.Name = "rdClient";
+            this.rdClient.Size = new System.Drawing.Size(55, 16);
+            this.rdClient.TabIndex = 11;
+            this.rdClient.TabStop = true;
+            this.rdClient.Text = "Client";
+            this.rdClient.UseVisualStyleBackColor = true;
+            // 
+            // txtRecv
+            // 
+            this.txtRecv.Location = new System.Drawing.Point(35, 71);
+            this.txtRecv.Multiline = true;
+            this.txtRecv.Name = "txtRecv";
+            this.txtRecv.Size = new System.Drawing.Size(763, 154);
+            this.txtRecv.TabIndex = 12;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(35, 266);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(377, 21);
+            this.txtSend.TabIndex = 13;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(447, 264);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 14;
+            this.btnSend.Text = "전송";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 310);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtRecv);
+            this.Controls.Add(this.rdClient);
+            this.Controls.Add(this.rbServer);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.txtIPaddr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Socket_test";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIPaddr;
+        private System.Windows.Forms.Label port;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RadioButton rbServer;
+        private System.Windows.Forms.RadioButton rdClient;
+        private System.Windows.Forms.TextBox txtRecv;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Socket_Test/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets; 
+
+namespace Socket_Test
+{
+    public partial class Form1 : Form
+    {
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            //// rb server 가 선택되도록 
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+            if(rbServer.Checked == true)
+            {
+                /// server 연결과정
+
+            }
+            else
+            {
+                /// client 연결과정
+
+
+            }
+
+            MessageBox.Show("연결!!!!");
+            //// 서버 socket bind listen 
+
+            //// 클라이언트 socket connect 
+
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //송수신 하는 단계  
+        }
+    }
+}
